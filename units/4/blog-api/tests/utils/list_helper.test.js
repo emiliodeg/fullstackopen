cfe3d7c27552f955432cd2955bbbfa99739fb32f
Emilio Degiovanni <emilio_degiovanni@ryder.com>
2024-07-27 14:51:55 -0300

    4.7: mostLikes tests

--- units/4/blog-api/tests/utils/list_helper.test.js
+++ units/4/blog-api/tests/utils/list_helper.test.js
@@ -147,3 +147,31 @@ describe("most blogs", () => {
     assert.deepStrictEqual(result, { author: "Robert C. Martin", blogs: 3 });
   });
 });
+
+describe("most likes", () => {
+  test("of empty list is null", () => {
+    const result = listHelper.mostLikes([]);
+    assert.strictEqual(result, null);
+  });
+  test("when list has only one blog, equals the likes of that", () => {
+    const listWithOneBlog = [
+      {
+        _id: "5a422aa71b54a676234d17f8",
+        title: "Go To Statement Considered Harmful",
+        author: "Edsger W. Dijkstra",
+        url: "https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf",
+        likes: 5,
+        __v: 0,
+      },
+    ];
+    const result = listHelper.mostLikes(listWithOneBlog);
+
+    assert.deepStrictEqual(result, { author: "Edsger W. Dijkstra", likes: 5 });
+  });
+
+  test("when list has many blogs", () => {
+    const result = listHelper.mostLikes(longListOfBlogs);
+    
+    assert.deepStrictEqual(result, { author: "Edsger W. Dijkstra", likes: 17 });
+  });
+})
--- units/4/blog-api/utils/list_helper.js
+++ units/4/blog-api/utils/list_helper.js
@@ -36,9 +36,30 @@ const mostBlogs = (blogs) => {
   };
 };
 
+const mostLikes = (blogs) => {
+  if (blogs.length === 0) return null;
+
+  const authors = {};
+  for (const blog of blogs) {
+    authors[blog.author] = (authors[blog.author] ?? 0) + blog.likes;
+  }
+
+  let mostLikes = Object.entries(authors)[0];
+
+  for (const [author, likes] of Object.entries(authors)) {
+    if (likes > mostLikes[1]) mostLikes = [author, likes];
+  }
+
+  return {
+    author: mostLikes[0],
+    likes: mostLikes[1],
+  };
+};
+
 module.exports = {
   dummy,
   totalLikes,
   favoriteBlog,
   mostBlogs,
+  mostLikes,
 };
