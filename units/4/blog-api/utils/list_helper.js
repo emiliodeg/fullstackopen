3ded2c613fda90bf37da91c38acd16562ad70413
Emilio Degiovanni <emilio_degiovanni@ryder.com>
2024-07-27 14:41:13 -0300

    4.5: favoriteBlog tests

--- units/4/blog-api/utils/list_helper.js
+++ units/4/blog-api/utils/list_helper.js
@@ -4,7 +4,20 @@ const dummy = (blogs) => {
 
 const totalLikes = (blogs) => blogs.reduce((sum, { likes }) => sum + likes, 0);
 
+const favoriteBlog = (blogs) => {
+  if (blogs.length === 0) return null;
+
+  let mostLiked = blogs[0];
+
+  for (const blog of blogs) {
+    if (blog.likes > mostLiked.likes) mostLiked = blog;
+  }
+
+  return mostLiked;
+};
+
 module.exports = {
   dummy,
   totalLikes,
+  favoriteBlog,
 };
