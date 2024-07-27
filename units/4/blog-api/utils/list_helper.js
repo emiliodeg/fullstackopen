b53e2c8c8dcf4c5be6b5475c7d8456dc56b5371e
Emilio Degiovanni <emilio_degiovanni@ryder.com>
2024-07-27 14:32:23 -0300

    4.4: totalLikes test

--- units/4/blog-api/utils/list_helper.js
+++ units/4/blog-api/utils/list_helper.js
@@ -1,7 +1,10 @@
 const dummy = (blogs) => {
-  return 1
+  return 1;
 };
+
+const totalLikes = (blogs) => blogs.reduce((sum, { likes }) => sum + likes, 0);
 
 module.exports = {
   dummy,
+  totalLikes,
 };
