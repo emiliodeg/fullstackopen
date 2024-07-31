bc8d6d7a3fa5642ecfdee2f35bb35a3daec25e2c
Emilio Degiovanni <emilio_degiovanni@ryder.com>
2024-07-31 10:16:17 -0300

    5.7: blog details

--- units/5/bloglist-frontend/src/components/Blog.jsx
+++ units/5/bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,22 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from "react";
 
-export default Blog
+const Blog = ({ blog }) => {
+  const [visible, setVisible] = useState(false);
+
+  return (
+    <div className="bordered">
+      {blog.title} <em>{blog.author}</em> <button onClick={() => setVisible(!visible)}>{visible ? "hide" : "view"}</button>
+      {visible && (
+        <div>
+          <a href={blog.url} target="_blank" rel="noreferrer noopenner">
+            {blog.url}
+          </a>
+          <br />
+          likes {blog.likes} <button>like</button>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Blog;
