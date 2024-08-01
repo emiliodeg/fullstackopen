25ff5ea1cf9e3cf1d6343456deaa93233c239249
Emilio Degiovanni <emilio_degiovanni@ryder.com>
2024-08-01 09:32:07 -0300

    5.14: click event and check url and likes

--- units/5/bloglist-frontend/src/components/Blog.test.jsx
+++ units/5/bloglist-frontend/src/components/Blog.test.jsx
@@ -1,4 +1,5 @@
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import Blog from './Blog'
 
 test('renders title and author', () => {
@@ -13,4 +14,28 @@ test('renders title and author', () => {
 
   screen.getByText(blog.title)
   screen.getByText(blog.author)
+})
+
+test('show url and number of likes', async () => {
+  const blog = {
+    title: 'Component testing is done with react-testing-library',
+    author: 'author',
+    url: 'https://url.test',
+    likes: 13,
+  }
+
+  render(<Blog blog={blog} />)
+
+  const url = screen.queryByText(blog.url)
+  const likes = screen.queryByText(blog.likes)
+
+  expect(url).toBeNull()
+  expect(likes).toBeNull()
+
+  const user = userEvent.setup()
+  const button = screen.getByText('view')
+  await user.click(button)
+
+  screen.getByText(blog.url)
+  screen.getByText(blog.likes, { exact: false })
 })
