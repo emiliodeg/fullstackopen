dd72365c70761b2e098c6ddd5ed752268db2cbb5
Emilio Degiovanni <emilio_degiovanni@ryder.com>
2024-08-15 10:45:39 -0300

    6.20: useQuery implementation

---
+++ units/6/query-anecdotes/src/App.jsx
@@ -0,0 +1,46 @@
+import AnecdoteForm from "./components/AnecdoteForm";
+import Notification from "./components/Notification";
+
+import { useQuery } from "@tanstack/react-query";
+import { getAnecdotes } from "./requests/anecdotes";
+
+const App = () => {
+  const { isError, isLoading, data: anecdotes } = useQuery({
+    queryKey: ["anecdotes"],
+    queryFn: getAnecdotes,
+    retry: 1,
+  });
+
+  if (isError) {
+    return <div>anecdote service not available due to problems in server</div>;
+  }
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  const handleVote = (anecdote) => {
+    console.log("vote");
+  };
+
+  return (
+    <div>
+      <h3>Anecdote app</h3>
+
+      <Notification />
+      <AnecdoteForm />
+
+      {anecdotes.map((anecdote) => (
+        <div key={anecdote.id}>
+          <div>{anecdote.content}</div>
+          <div>
+            has {anecdote.votes}
+            <button onClick={() => handleVote(anecdote)}>vote</button>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default App;
